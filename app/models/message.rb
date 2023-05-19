38a137c88885b5173357ff544bd6fb89c4637025
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-19 11:17:42 -0500

    Fix rubocop offenses.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   def as_json(options = {})
-  super(options.merge(
-    only: %i[id message]
-  ))
+    super(options.merge(
+      only: %i[id message]
+    ))
   end
 end
