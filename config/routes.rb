524c118cce90813ff74520ac4f9ab0397fd773ce
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-19 08:55:06 -0500

    Add the /messages endpoint to get random messages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :messages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "messages#show"
+  get '/messages', to: 'messages#show'
 end
